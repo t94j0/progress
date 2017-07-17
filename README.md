68bb14742ed89eab55adaa0431d03e5aa6dbf0f9
Max Harley <augeocacher@gmail.com>
2017-07-16 23:46:55 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # progress
-Progress bar for creating super l33t numerical progress bars in golang
+
+Progress is a new standard for how CLI application progress bars should behave. This is a super innovative and game-changing CLI UI library.
+
+Totally joking...
+
+There are two different progress bars that can be made. The first is when the maximum value is known:
+![Hack the planet!](./assets/hack_russia.gif)
+
+And one where the maximum value is not known
+
+![](./assets/parse_files.gif)
+
+The code is very simple. `hack_russia.go` is shown below:
+
+
+```
+package main
+
+import (
+        "time"
+
+        "github.com/t94j0/progress"
+)
+
+const Max = 10
+
+func main() {
+        tracker := progress.CreateTrackerMax("Hacking Russia", Max)
+        for i := 0; i < Max; i++ {
+                time.Sleep(time.Second * 1)
+                tracker.Increment()
+        }
+}
+```
+
+and `parse_files.go` is shown here:
+
+```
+package main
+
+import (
+        "time"
+
+        "github.com/t94j0/progress"
+)
+
+func main() {
+        tracker := progress.CreateTracker("Files")
+        for i := 0; i < 5; i++ {
+                tracker.Increment()
+                time.Sleep(time.Second)
+        }
+}
+```
--- progress.go
+++ progress.go
@@ -2,30 +2,44 @@ package progress
 
 import "fmt"
 
+// ProgressTracker holds information about how far the tracker has gotten by
+// keeping track of the current count and the maximum. The `Comment` field
+// indicates what string should be presented when incrementing
 type ProgressTracker struct {
 	Comment string
 	Count   int
 	Maximum int
 }
 
-func CreateProgressTracker(comment string) *ProgressTracker {
+// CreateTracker creates a ProgressTracker object that has a `Maximum` of -1 to
+// signify that the maxiumum value is unknown.
+func CreateTracker(comment string) *ProgressTracker {
 	return &ProgressTracker{comment, 0, -1}
 }
 
-func CreateProgressTrackerMax(comment string, max int) *ProgressTracker {
+// CreateTrackerMax creates a tracker where the `Maximum` field known. This
+// changes how the printing is done
+func CreateTrackerMax(comment string, max int) *ProgressTracker {
 	return &ProgressTracker{comment, 0, max}
 }
 
+// Increment simply increments the counter on ProgressTracker
 func (p *ProgressTracker) Increment() {
 	p.Count += 1
+	p.print()
+}
+
+// print prints the count according to if the Maximum is known or not
+func (p *ProgressTracker) print() {
 	if p.Maximum == -1 {
 		fmt.Printf("\r%s: %d", p.Comment, p.Count)
 	} else {
 		fmt.Printf("\r%s: (%d/%d)", p.Comment, p.Count, p.Maximum)
 	}
-
 }
 
+// Close is used to create a newline so that the previous string in stdout is
+// not written over
 func (p *ProgressTracker) Close() {
 	fmt.Print("\n")
 }
